bdce1937e324fe60dfe705b86d54121d4a2f36ce
kimanh <kimanh3082@gmail.com>
2022-10-09 21:00:16 +0700

    test

--- src/main/java/com/minswap/hrms/service/ServiceTest.java
+++ src/main/java/com/minswap/hrms/service/ServiceTest.java
@@ -1,13 +1,16 @@
 package com.minswap.hrms.service;
 
+import com.minswap.hrms.model.Role;
 import org.springframework.http.ResponseEntity;
 
 import com.minswap.hrms.exception.model.Pageable;
 import com.minswap.hrms.model.BaseResponse;
 import com.minswap.hrms.model.TestResponse;
 
+import java.util.List;
+
 public interface ServiceTest {
   
-  ResponseEntity<BaseResponse<TestResponse, Pageable>> serviceTest(String text);
+  ResponseEntity<BaseResponse<List<Role>, Pageable>> serviceTest();
 
 }
--- src/main/java/com/minswap/hrms/service/impl/ServiceTestImpl.java
+++ src/main/java/com/minswap/hrms/service/impl/ServiceTestImpl.java
@@ -1,5 +1,8 @@
 package com.minswap.hrms.service.impl;
 
+import com.minswap.hrms.model.Role;
+import com.minswap.hrms.repository.RoleRespository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -10,23 +13,23 @@ import com.minswap.hrms.service.ServiceTest;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.List;
+
 @Service
 @Slf4j
 public class ServiceTestImpl implements ServiceTest{
-
+  @Autowired
+  RoleRespository roleRespository;
   @Override
-  public ResponseEntity<BaseResponse<TestResponse, Pageable>> serviceTest(String requestId) {
+  public ResponseEntity<BaseResponse<List<Role>, Pageable>> serviceTest() {
     
     Pageable pageable = new Pageable();
     pageable.setLimit(20);
     pageable.setPage(13);
     pageable.setRecords(5);
     pageable.setTotalRecords(2);
-    TestResponse response = new TestResponse();
-    response.setRequestId(requestId);
-    response.setTestAccount("abc test");
-     ResponseEntity<BaseResponse<TestResponse, Pageable>> responseEntity =
-        BaseResponse.ofSucceededOffset(response, pageable);
+    ResponseEntity<BaseResponse<List<Role>, Pageable>> responseEntity =
+        BaseResponse.ofSucceededOffset(roleRespository.findAll(), pageable);
     return responseEntity;
   }
 
