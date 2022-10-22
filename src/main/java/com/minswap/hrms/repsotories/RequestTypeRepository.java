3b0c000d432e44e365919dab4b0d3e933c47e62b
HuyNS <huynshe141505@fpt.edu.vn>
2022-10-22 13:27:56 +0700

    API search request

--- src/main/java/com/minswap/hrms/repsotories/RequestTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.minswap.hrms.repsotories;
-
-import com.minswap.hrms.entities.RequestType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RequestTypeRepository extends JpaRepository<RequestType,Integer> {
-}
