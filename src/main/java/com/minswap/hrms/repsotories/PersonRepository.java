05a98661c62c3db7deb95793717559de24dda567
HuyNS <huynshe141505@fpt.edu.vn>
2022-10-22 13:29:15 +0700

    API search request

---
+++ src/main/java/com/minswap/hrms/repsotories/PersonRepository.java
@@ -0,0 +1,14 @@
+package com.minswap.hrms.repsotories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.minswap.hrms.entities.Person;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long>{
+
+    Optional<Person> findPersonByPersonId(Integer id);
+}
