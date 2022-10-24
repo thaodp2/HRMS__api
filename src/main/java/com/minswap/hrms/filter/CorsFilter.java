2b2106330cfaa1d58dab882b2215c688408fcdb6
thao.dinh <phuongthaoybyb@gmail.com>
2022-10-24 21:36:04 +0700

    UPDATE: Add CORSFilter action

--- src/main/java/com/minswap/hrms/filter/CorsFilter.java
+++ src/main/java/com/minswap/hrms/filter/CorsFilter.java
@@ -1,17 +1,5 @@
 package com.minswap.hrms.filter;
 
-import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,16 +7,17 @@ import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 /**
  * Created by phucnguyen on 09/03/2017.
  */
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
-@Slf4j
 public class CorsFilter implements Filter {
 
 	/** The logger. */
@@ -60,12 +49,14 @@ public class CorsFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpServletRequest request = (HttpServletRequest) req;
 		try {
-		ApiKeyVerifiRequestWrapper requestWrapper = new ApiKeyVerifiRequestWrapper(request);
-		JSONParser parser = new JSONParser();
-		JSONObject dataRequest = StringUtils.isEmpty(requestWrapper.getBody()) ? new JSONObject()
-				: (JSONObject) parser.parse(requestWrapper.getBody());
-		requestWrapper.setBody(dataRequest.toString());
-			chain.doFilter(requestWrapper, res);
+			ApiKeyVerifiRequestWrapper requestWrapper = new ApiKeyVerifiRequestWrapper(request);
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.setHeader("Access-Control-Allow-Credentials", "true");
+			response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+			response.setHeader("Access-Control-Max-Age", "3600");
+			response.setHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Authorization, Origin, Accept, Access-Control-Request-Method, Access-Control-Request-Headers");
+
+			chain.doFilter(req, res);
 		} catch (Exception e) {
 			e.printStackTrace();
 			response.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
