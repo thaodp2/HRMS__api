d3b3b36fd84fc2d94eb80cda85ae51f3c3ed4cea
thao.dinh <phuongthaoybyb@gmail.com>
2022-12-09 16:54:58 +0700

    fix(search employee): update roll manager

--- src/main/java/com/minswap/hrms/controller/manager/ManagerPersonController.java
+++ src/main/java/com/minswap/hrms/controller/manager/ManagerPersonController.java
@@ -3,6 +3,8 @@ package com.minswap.hrms.controller.manager;
 import com.minswap.hrms.constants.CommonConstant;
 import com.minswap.hrms.model.BaseResponse;
 import com.minswap.hrms.response.EmployeeInfoResponse;
+import com.minswap.hrms.security.UserPrincipal;
+import com.minswap.hrms.security.oauth2.CurrentUser;
 import com.minswap.hrms.service.person.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -26,11 +28,12 @@ public class ManagerPersonController {
           @RequestParam (name = "rollNumber", required = false) String rollNumber,
           @RequestParam (name = "active", required = false) String active,
           @RequestParam (name = "positionId", required = false)Long positionId,
-          @RequestParam (name = "managerRoll", required = false)String managerRoll,
+          @CurrentUser UserPrincipal userPrincipal,
           @RequestParam(required = false) String sort,
           @RequestParam(required = false) String dir
 
   ) {
+	 String managerRoll = personService.getPersonInforByEmail(userPrincipal.getEmail()).getRollNumber();
     return personService.getSearchListEmployee(page,limit,fullName,email,departmentId,rollNumber,active,positionId,managerRoll, sort, dir);
   }
 }
