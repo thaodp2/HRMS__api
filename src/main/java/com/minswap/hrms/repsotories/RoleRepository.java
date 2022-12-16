b69096befa39d787f1d3b7b6191aa7ee4d6f9636
kimanh <kimanh3082@gmail.com>
2022-12-16 22:41:21 +0700

    add annotation transactional

--- src/main/java/com/minswap/hrms/repsotories/RoleRepository.java
+++ src/main/java/com/minswap/hrms/repsotories/RoleRepository.java
@@ -3,9 +3,13 @@ package com.minswap.hrms.repsotories;
 import com.minswap.hrms.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-
+@Repository
+@Transactional
 public interface RoleRepository extends JpaRepository<Role, Long>, JpaSpecificationExecutor<Role> {
 
     List<Role> findByRoleIdIn(List<Long> roleIds);
